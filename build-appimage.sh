c8c2e8577c6728f3073fa07ccc96ecef5468ca0c
Ma5t3rful <37829730+Ma5t3rful@users.noreply.github.com>
2025-06-27 20:45:24 +0330

    For now a simple build script for AppImage

---
+++ build-appimage.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+pushd build
+cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release
+cmake --build . --config Release
+popd
+pushd AppDir
+linuxdeploy-x86_64.AppImage -e build/proxy-master -d misc/proxy-master.desktop -i misc/proxy-master.png --appdir AppDir
+cp build/dark.txt AppDir/usr/bin/
+linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage
