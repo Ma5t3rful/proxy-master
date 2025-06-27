c0793476d1473442056cfec271eddc7b60001466
Ma5t3rful <37829730+Ma5t3rful@users.noreply.github.com>
2025-06-27 22:01:24 +0330

    Better arg parser with less allocations

--- src/main.cpp
+++ src/main.cpp
@@ -9,32 +9,30 @@
 #include "master_window.hpp"
 #include "sdl_backend_helper.hpp"
 
-
-void parse_arg (std::string* const arg)
+std::string parse_arg (const std::string_view arg)
 {
-    if(arg->starts_with("--"))
-    {
-        arg->erase(0,2);
-        return;
+    if (arg.starts_with("--")) {
+        return std::string(arg.substr(2));
     }
     const std::unordered_map<std::string_view, std::string> flag_name{
         {"-h","help"},
         {"-r","reset"},
     };  
-    const auto find = flag_name.find(*arg);
-    if(find != flag_name.end())*arg = find->second;
+    if(const auto find = flag_name.find(arg);find != flag_name.end())
+        return find->second;
+    return std::string(arg);
 }
+
 
 auto main (int argc,char**argv) -> int
 {
     ProxyHandler proxy;
     if(argc>1)
     {
-        std::string main_arg = argv[1];
-        parse_arg(&main_arg);
+        const auto main_argv = parse_arg(argv[1]);
         std::println("Proxy Master command line mode:");
-        if(main_arg == "help")std::println("USAGE: proxy-master [COMMAND] [ARGS]...");
-        else if(main_arg == "set")
+        if(main_argv == "help")std::println("USAGE: proxy-master [COMMAND] [ARGS]...");
+        else if(main_argv == "set")
         {
             if(argc != 4)
             {
@@ -44,9 +42,8 @@ auto main (int argc,char**argv) -> int
             if(const auto result = proxy.set(argv[2], argv[3]);result)std::println("Proxy is set!");
             else std::println("Error: {}",result.error());
         }
-        else if(main_arg == "check") std::println("Proxy is {}",proxy.is_on()?"ON":"OFF");
+        else if(main_argv == "check") std::println("Proxy is {}",proxy.is_on()?"ON":"OFF");
         else std::println("Unknown command");
-        //std::println("Current state: {}",proxy.is_on()?"ON":"OFF");
         return 0;
     }
     
