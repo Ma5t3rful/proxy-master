9f0794bcd909bb32bab8badde1bb1bb47fcb1297
Ma5t3rful <37829730+Ma5t3rful@users.noreply.github.com>
2025-06-30 13:35:35 +0330

    Fix AppImage build script

--- build-appimage.sh
+++ build-appimage.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 rm -fr AppDir
+mkdir -p build
 pushd build
 cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release
 cmake --build . --config Release
 popd
-pushd AppDir
-linuxdeploy-x86_64.AppImage -e build/proxy-master -d misc/proxy-master.desktop -i misc/proxy-master.png --appdir AppDir
+linuxdeploy-x86_64.AppImage -e build/proxy-master -d misc/proxy-master.desktop -i misc/proxy-master.png --appdir AppDir/
 cp build/dark.txt AppDir/usr/bin/
 linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage
